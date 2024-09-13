4698ff9165dd43021b4619ce6744ac0c88241d3b
tsunami2 <101075766+tsunami2@users.noreply.github.com>
2024-09-12 22:04:45 -0700

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,7 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const TWITTER_LINK = "https://twitter.com/lil2krickky";
 export const INSTAGRAM_LINK = "https://instagram.com/lil2krickky"
+export const DONATION_LINK = "buymeacoffee.com/loscherriesgang"
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // Do it clap??
